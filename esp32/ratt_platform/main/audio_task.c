31465041294546e3f9c7e19c36269f8d910af9d8
Steve Richardson <tangentaudio@gmail.com>
2017-02-08 18:22:39 -0800

    i2s audio work in progress, streams s16 format audio from SD card in background still a bit flaky, not entirely sure SD card library is task-safe?

---
+++ esp32/ratt_platform/main/audio_task.c
@@ -0,0 +1,73 @@
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_system.h"
+#include "driver/i2s.h"
+#include "esp_log.h"
+#include "math.h"
+
+static const char *TAG = "audio_task";
+
+#define SAMPLE_RATE     (16000)
+#define I2S_NUM         (0)
+
+QueueHandle_t m_qhandle;
+
+void audio_init()
+{
+    i2s_config_t i2s_config = {
+        .mode = I2S_MODE_MASTER | I2S_MODE_TX,
+        .sample_rate = SAMPLE_RATE,
+        .bits_per_sample = 16,
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .communication_format = I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB,
+        .dma_buf_count = 4,
+        .dma_buf_len = 512,
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1
+    };
+    i2s_pin_config_t pin_config = {
+        .bck_io_num = 26,
+        .ws_io_num = 25,
+        .data_out_num = 22,
+        .data_in_num = -1
+    };
+
+    i2s_driver_install(I2S_NUM, &i2s_config, 64, &m_qhandle);
+    i2s_set_pin(I2S_NUM, &pin_config);    
+}
+
+
+
+void audio_task(void *pvParameters)
+{
+    i2s_event_type_t evt;
+
+    FILE* f = fopen("/sdcard/smb.s16", "r");
+    char buf[512];
+    
+    while (1) {
+        int r;
+        do {
+            fread(buf, sizeof(buf), 1, f);
+            r = i2s_write_bytes(I2S_NUM, buf, sizeof(buf), portMAX_DELAY);
+        } while (r>0);
+
+        if (xQueueReceive(m_qhandle, &evt, 0) == pdTRUE) {
+            switch (evt) {
+            case I2S_EVENT_DMA_ERROR:
+                ESP_LOGE(TAG, "I2S_EVENT_DMA_ERROR");
+                break;
+            case I2S_EVENT_TX_DONE:
+                ESP_LOGI(TAG, "I2S_EVENT_TX_DONE");
+                break;
+            case I2S_EVENT_RX_DONE:
+                ESP_LOGI(TAG, "I2S_EVENT_RX_DONE");
+                break;
+            default:
+                ESP_LOGE(TAG, "I2S ERR unknown event");
+            }
+        }
+    }    
+}
---
+++ esp32/ratt_platform/main/audio_task.h
@@ -0,0 +1,10 @@
+#ifndef _AUDIO_TASK_H
+#define _AUDIO_TASK_H
+
+void audio_init();
+void audio_task(void *pvParameters);
+
+
+
+
+#endif
