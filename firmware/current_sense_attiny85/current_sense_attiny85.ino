6364a381e924811c41b654f36aa4a8c3d2443f33
tangentaudio <tangentaudio@users.noreply.github.com>
2019-04-02 22:49:39 -0400

    start of softserial-based calibration routine for current sensor

--- firmware/current_sense_attiny85/current_sense_attiny85.ino
+++ firmware/current_sense_attiny85/current_sense_attiny85.ino
@@ -42,34 +42,48 @@
 //
 // programmed via ICSP pins using AVR Dragon or other ICSP tools
 
+#include <SoftwareSerial.h>
+
 #define PIN_OUT 4
 #define PIN_ANALOG_IN 3
-#define PIN_LED 1
 
-#define AVG_SIZE 256
-#define DEAD_BAND 8
+#define WIN_SIZE 196
+#define DEAD_BAND 16
 #define MIN_COUNT 64
+#define CENTER_VALUE 516
+
+#define PIN_SERIAL_OUT 1
+#define PIN_SERIAL_IN 0
+
+static char window[WIN_SIZE];
+
+
+SoftwareSerial softSerial(PIN_SERIAL_IN, PIN_SERIAL_OUT);
 
 void setup() {
+  softSerial.begin(9600);
+  
   analogReference(DEFAULT);
   pinMode(PIN_OUT, OUTPUT);
   pinMode(PIN_ANALOG_IN, INPUT);
-  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_SERIAL_OUT, OUTPUT);
+
+  softSerial.println("RATT Current Sensor");
+  softSerial.println("ESC 3 times to calibrate.");
 }
 
 int do_avg(int in)
 {
-  static bool window[AVG_SIZE];
   static int pos = 0;
 
-  window[pos] = (in > (512 + DEAD_BAND)) | (in < (512 - DEAD_BAND));
+  window[pos] = (in > (CENTER_VALUE + DEAD_BAND)) | (in < (CENTER_VALUE - DEAD_BAND));
   
   pos++;
-  if (pos >= AVG_SIZE)
+  if (pos >= WIN_SIZE)
     pos = 0;
 
   int sum = 0;
-  for (unsigned int i=0; i< AVG_SIZE; i++) {
+  for (unsigned int i=0; i< WIN_SIZE; i++) {
     if (window[i])
       sum++;
   }
@@ -77,11 +91,127 @@ int do_avg(int in)
   return sum;
 }
 
-void loop() {
+void run() {
   int ain = analogRead(PIN_ANALOG_IN);
   int count = do_avg(ain);
-
   bool on = (count >= MIN_COUNT);
 
   digitalWrite(PIN_OUT, on ? HIGH : LOW);
+}
+
+
+bool get_char (char *c) {
+  if (softSerial.available()) {
+    *c = softSerial.read();
+    return true;
+  }
+  return false;
+}
+
+#define IWIN_SIZE (WIN_SIZE / 2)
+void cal_off_level()
+{
+  unsigned int* iwindow = (unsigned int*) &window;
+  unsigned int min = 65535;
+  unsigned int max = 0;
+
+  softSerial.println(F("Acquiring..."));
+  for (unsigned int i=0; i < IWIN_SIZE; i++) {
+    unsigned int in = analogRead(PIN_ANALOG_IN);
+    delay(5);
+    iwindow[i] = in;
+  }
+
+  softSerial.println(F("Calculating..."));
+  unsigned long accum = 0;
+  for (unsigned int i=0; i < IWIN_SIZE; i++) {
+    unsigned int v = iwindow[i];
+    
+    if (v < min)
+      min = v;
+
+    if (v > max)
+      max = v;
+
+    accum += v;
+  }
+
+  unsigned int avg = (accum / IWIN_SIZE);
+
+  softSerial.print(F("Window Size="));
+  softSerial.println(IWIN_SIZE);
+  softSerial.print(F("AVG="));
+  softSerial.println(avg);
+  softSerial.print(F("MIN="));
+  softSerial.println(min);
+  softSerial.print(F("MAX="));
+  softSerial.println(max);
+  softSerial.print(F("DEADBAND="));
+  softSerial.println(max - min);
+  softSerial.println(F("Cal done."));
+}
+
+
+#define STATE_CAL_INIT 0
+#define STATE_CAL_WAIT_MODE 1
+
+bool cal() {
+  static unsigned char state = STATE_CAL_INIT;
+  char c;
+  
+  switch (state) {
+  case STATE_CAL_INIT:
+    softSerial.println(F("Calibrate: 1-OFF Level | 2-Idle Level | 3-On Level | Q-Quit"));
+    state = STATE_CAL_WAIT_MODE;
+    break;
+  case STATE_CAL_WAIT_MODE:
+    if (get_char(&c)) {
+      switch (c) {
+      case '1':
+        cal_off_level();
+        state = STATE_CAL_INIT;
+        break;
+        
+      case 'q':
+      case 'Q':
+        softSerial.println("Quit");
+        state = STATE_CAL_INIT;
+        return false;
+      }
+    }
+    break;
+  }
+
+  return true;
+}
+
+#define STATE_RUN 0
+#define STATE_CAL 1
+
+void loop() {
+  static unsigned char state = STATE_RUN;
+  static unsigned char esc_count = 0;
+  
+  switch (state) {
+  case STATE_RUN:
+    run();
+    if (softSerial.available()) {
+      char c = softSerial.read();
+
+      if (c == 27) {
+        esc_count++;
+        if (esc_count >= 3) {
+          state = STATE_CAL;
+          esc_count = 0;
+        }
+      }
+    }
+    
+    break;
+  case STATE_CAL:
+    if (!cal())
+      state = STATE_RUN;
+    break;
+  }
+
 }
